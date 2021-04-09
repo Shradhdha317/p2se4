267e3b58a4332cf19d5899e5ef5fb16feb42544a
Shradhdha317 <74590893+Shradhdha317@users.noreply.github.com>
2021-04-09 09:28:20 -0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,30 @@
+<?php
+
+function get_price($product, $price)
+{
+
+		$host = "tcp:localhost17.database.windows.net,1433";
+		$user = "patels18@localhost17";
+		$pwd  = "Cloudif9bre";
+		$db   = "patels18_db";
+	    	$conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
+	    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$sql = ("SELECT * FROM products WHERE product = '$product' and price = '$price'");
+		  	$sql_run = $conn->query($sql);
+	$exe = $sql_run->rowCount();
+	
+
+	if ($exe == 0)
+	 $y = "false";
+	else
+	    {
+	    return true;
+		}
+		return $y;
+	  $conn = NULL;
+}
+
+?>
+
+
