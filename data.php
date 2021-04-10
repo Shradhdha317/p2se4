f4a9b7af5c47f3bc77c6c67cdab90bb06106b0d8
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-10 17:38:50 -0400

    try

--- data.php
+++ data.php
@@ -3,42 +3,37 @@
 function get_price($product, $price)
 {
 
-
 	$con=mysqli_init(); 
-	mysqli_ssl_set($con, NULL, NULL, "/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
-	mysqli_real_connect($con, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18", 3306);
+	mysqli_ssl_set($con, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_real_connect($con, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
 
-       $sql = "SELECT price FROM products WHERE product = '$product'";
-       $result = $con->query($sql);
+       $sql = ("SELECT price FROM products WHERE product = '$product'");
+	$result = $conn->query($sql);
+	$exe = $result->rowcount();
+	$row = $result->fetch();
 
-        if ($result->num_rows > 0) 
+        if ($exe == 0) 
 	{
-
-             while($row = $result->fetch_assoc()) 
-	 	 {
-                      $p = $row["price"];
-      	  	}
+		$p = null;
+		return NULL;
     	}
 	 else
-		 {
-                     $p = null;
-		     return NULL;	
+		{
+                    $p = $row["price"];	
         	}
+	$conn = NULL;
 
-    $con->close();
+	if ($p == $price) 
+  	{
+    		return "true";
+  	}
+	else 
+ 	{
+  		return "false";
 
-if ($p == $price) 
-  {
-    return "true";
-  }
-else 
- {
-  return "false";
-
- }
+	}
 
 }
-
 ?>
 
 
