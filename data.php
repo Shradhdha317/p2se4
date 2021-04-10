0fe9ecfb476c5acd8a620b5219b44b2420c3ec23
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-10 17:17:38 -0400

    changed connection

--- data.php
+++ data.php
@@ -1,42 +1,44 @@
 <?php
-
+		
 function get_price($product, $price)
 {
 
-		$host = "tcp:localhost17.database.windows.net,1433";
-		$user = "patels18@localhost17";
-		$pwd  = "Cloudif9bre";
-		$db   = "patels18_db";
-	    	$conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
-	    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
-		$sql = ("SELECT price FROM products WHERE product = '$product'");
-	$result = $conn->query($sql);
-	$exe = $result->rowcount();
-	$row = $result->fetch();
 
-        if ($exe == 0) 
+	$con=mysqli_init(); 
+	mysqli_ssl_set($con, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
+	mysqli_real_connect($con, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
+
+       $sql = "SELECT price FROM products WHERE product = '$product'";
+       $result = $con->query($sql);
+
+        if ($result->num_rows > 0) 
 	{
-		$p = null;
-		return NULL;
+
+             while($row = $result->fetch_assoc()) 
+	 	 {
+                      $p = $row["price"];
+      	  	}
     	}
 	 else
-		{
-                    $p = $row["price"];	
+		 {
+                     $p = null;
+		     return NULL;	
         	}
-	$conn = NULL;
 
-	if ($p == $price) 
-  	{
-    		return "true";
-  	}
-	else 
- 	{
-  		return "false";
+    $con->close();
 
-	}
+if ($p == $price) 
+  {
+    return "true";
+  }
+else 
+ {
+  return "false";
+
+ }
 
 }
+
 ?>
 
 
