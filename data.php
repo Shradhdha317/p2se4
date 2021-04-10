a450e1ed34f14bf48199e492a9be402ea8c7657e
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-10 17:44:28 -0400

    push

--- data.php
+++ data.php
@@ -7,33 +7,37 @@ function get_price($product, $price)
 	mysqli_ssl_set($con, NULL, NULL, "C:/Users/shrad/Desktop/Github for cloud management/DigiCertGlobalRootG2.crt.pem", NULL, NULL); 
 	mysqli_real_connect($con, "patels18.mysql.database.azure.com", "patels18@patels18", "Krishna@0115", "patels18_db", 3306);
 
-       $sql = ("SELECT price FROM products WHERE product = '$product'");
-	$result = $con->query($sql);
-	$exe = $result->rowcount();
-	$row = $result->fetch();
+       $sql = "SELECT price FROM products WHERE product = '$product'";
+       $result = $conn->query($sql);
 
-        if ($exe == 0) 
+        if ($result->num_rows > 0) 
 	{
-		$p = null;
-		return NULL;
+
+             while($row = $result->fetch_assoc()) 
+	 	 {
+                      $p = $row["price"];
+      	  	}
     	}
 	 else
-		{
-                    $p = $row["price"];	
+		 {
+                     $p = null;
+		     return NULL;	
         	}
-	$con = NULL;
 
-	if ($p == $price) 
-  	{
-    		return "true";
-  	}
-	else 
- 	{
-  		return "false";
+    $conn->close();
 
-	}
+if ($p == $price) 
+  {
+    return "true";
+  }
+else 
+ {
+  return "false";
+
+ }
 
 }
+
 ?>
 
 
